7b01276638cbbc29359c7303824e9e40201b6694
gabisegger <gabi.segger@gmail.com>
2022-09-15 20:38:56 -0300

    feat: criação e estilo do componente Footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import styles from './styles.module.css'
+
+export function Footer() {
+  return (
+
+    <footer className={styles.footer}>
+      <p>Testando suas habilidades em ReactJs.</p>
+      <p>Linx Impulse | Resilia Educação</p>
+      <p>2022</p>
+    </footer>
+
+  )
+}
