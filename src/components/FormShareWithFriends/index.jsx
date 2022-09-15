4af746cae176613d1006559c9d3598bd22393529
gabisegger <gabi.segger@gmail.com>
2022-09-15 20:43:44 -0300

    feat: criação e estilo do componente Formulário

---
+++ src/components/FormShareWithFriends/index.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import styles from './styles.module.css';
+
+export function FormShareWithFriends() {
+  const [inputValue, setInputValue] = useState('');
+
+  function handleValidateInputs(event) {
+    event.preventDefault();
+
+    const regexValidateEmail = /^\S+@\S+\.\S+$/;
+
+    if (!regexValidateEmail.test(inputValue)) {
+      alert('Insira um E-mail válido');
+      return;
+    }
+
+    alert('Compartilhamento feito com sucesso');
+    window.location.reload();
+  }
+
+  return (
+    <div>
+      <p className={styles.paragraph}>
+        Quer que seus amigos também ganhem a lista personalizada deles? Preencha
+        agora!
+      </p>
+
+      <form id='share' onSubmit={handleValidateInputs} className={styles.form}>
+        <div className={styles.inputsContainer}>
+          <div>
+            <label htmlFor="friendName">Nome do seu amigo: </label>
+            <input type="text" id="friendName" required />
+          </div>
+          <div>
+            <label htmlFor="emailFriend">E-mail:</label>
+            <input
+              type="text"
+              id="emailFriend"
+              onChange={e => setInputValue(e.target.value)}
+            />
+          </div>
+        </div>
+        <button type="submit">Enviar agora</button>
+      </form>
+    </div>
+  );
+}
