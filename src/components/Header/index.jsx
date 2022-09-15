ab06a2b6266de585c708314b8bb3684d5fba6466
gabisegger <gabi.segger@gmail.com>
2022-09-15 20:38:16 -0300

    feat: criação e estilo do componente Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,26 @@
+import styles from './styles.module.css';
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <strong>uma seleção de produtos</strong>
+      <h1>especial para você</h1>
+
+      <p>
+        Todos os produtos desta lista foram selecionados a partir da sua
+        navegação. Aproveite!
+      </p>
+
+      <footer className={styles.footer}>
+        <div>
+          <a href="https://www.linx.com.br/quem-somos/" target="_blank">Conheça a Linx</a>
+          <a href="#helpAlgoritm">Ajude o algorítimo</a>
+        </div>
+        <div>
+          <a href="#products">Seus produtos</a>
+          <a href="#share">Compartilhe</a>
+        </div>
+      </footer>
+    </header>
+  );
+}
